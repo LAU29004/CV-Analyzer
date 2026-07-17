c604f9ff7c2387440868711afdf17c84287b2a5f
Lokesh-Mahajan14 <lmahajan016@gmail.com>
2026-07-17 14:39:59 +0530

    Firebase

--- server/config/firebase-admin.js
+++ server/config/firebase-admin.js
@@ -1,17 +1,9 @@
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Read service account key
 const serviceAccount = JSON.parse(
-  readFileSync(join(__dirname, "serviceAccount.json"), "utf8")
+  process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
-// Initialize Firebase Admin
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
